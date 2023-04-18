cb029f45dab6ae1c0537234571c523b0564a71a7
Richard/NotDutchy <richard.elean@gmail.com>
2023-04-18 15:33:05 +0200

    Opdracht 3

--- CppOpdrachten/Opdracht3/Opdracht3.cpp
+++ CppOpdrachten/Opdracht3/Opdracht3.cpp
@@ -1,20 +1,37 @@
-// Opdracht3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+using namespace std;
+void count(const char s[], int counts[]);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char s[100];
+    int counts[26];
+    cout << "Enter a string: " << endl;
+    cin.getline(s, 100);
+
+    count(s, counts);
+    
+    for (int i = 0; i < 26; i++) {
+        if (counts[i] == 0) {
+            continue;
+        }
+        char c = 'a' + i;
+        cout << c << ": " << counts[i] << " times." << endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void count(const char s[], int counts[]) {
+    for (int i = 0; i < 26; i++) {
+        counts[i] = 0;
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (int i = 0; i < strlen(s); i++) {
+        char c = tolower(s[i]);
+        if (c >= 'a' && c <= 'z') {
+            int charIndex = c - 'a';
+            counts[charIndex]++;
+        }
+    }
+}
