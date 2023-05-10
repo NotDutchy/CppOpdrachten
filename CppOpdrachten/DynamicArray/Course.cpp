b98f33759fa1565140669da97a83e2dde98e37fc
Richard <r.elean@student.avans.nl>
2023-05-10 15:13:19 +0200

    More stuff

---
+++ CppOpdrachten/DynamicArray/Course.cpp
@@ -0,0 +1,59 @@
+#include "Course.h"
+#include <iostream>
+
+using namespace std;
+
+Course::Course(const std::string& courseName, int capacity) {
+	numberOfStudents = 0;
+	this->courseName = courseName;
+	this->capacity = capacity;
+	students = new std::string[capacity];
+}
+
+Course::Course(const Course& course) {
+	numberOfStudents = course.getNumberOfStudents();
+	this->courseName = course.courseName;
+	this->capacity = course.capacity;
+	//this->students = course.students;
+	this->students = new std::string[this->capacity];
+	for (int i = 0; i < this->capacity; i++) {
+		this->students[i] = course.students[i];
+	}
+}
+
+Course::~Course() {
+	delete[] students;
+}
+
+std::string Course::getCourseName() const {
+	return courseName;
+}
+
+void Course::addStudent(const std::string& name) {
+
+	if (numberOfStudents >= capacity) {
+		string* newStudents;
+		int newCapacity = capacity++;
+		newStudents = new string[newCapacity];
+		for (int i = 0; i < sizeof(students)/sizeof(students[0]); i++)
+		{
+			newStudents[i] = students[i];
+			cout << students[i] << "Has been placed into the new array" << newStudents[i] << endl;
+		}	
+		students = newStudents;
+	}
+	students[numberOfStudents] = name;
+	numberOfStudents++;
+}
+
+void Course::dropStudent(const std::string& name) {
+	// Left as an exercise
+}
+
+std::string* Course::getStudents() const {
+	return students;
+}
+
+int Course::getNumberOfStudents() const {
+	return numberOfStudents;
+}
---
+++ CppOpdrachten/DynamicArray/Course.h
@@ -0,0 +1,20 @@
+#pragma once
+class Course
+{
+public:
+	Course(const std::string& courseName, int capacity);
+	Course(const Course& course);
+	~Course();
+	std::string getCourseName() const;
+	void addStudent(const std::string& name);
+	void dropStudent(const std::string& name);
+	std::string* getStudents() const;
+	int getNumberOfStudents() const;
+
+private:
+	std::string courseName;
+	std::string* students;
+	int numberOfStudents;
+	int capacity;
+};
+
