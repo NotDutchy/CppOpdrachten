b138203c4954a714634ecaecda530afef92d2ba4
Richard/NotDutchy <richard.elean@gmail.com>
2023-05-10 16:50:46 +0200

    Finished opdracht 6

--- CppOpdrachten/CountVowels/CountVowels.cpp
+++ CppOpdrachten/CountVowels/CountVowels.cpp
@@ -1,20 +1,31 @@
-// CountVowels.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+
+    cout << "Enter a filename: \n";
+    string fileName;
+    cin >> fileName;
+
+    ifstream input_file(fileName);
+
+    if (!input_file) {
+        cout << "Error: file does not exist." << endl;
+        return 1;
+    }
+
+    int count = 0;
+    char c;
+    while (input_file.get(c)) {
+	    if (c == 'a' || c == 'e' || c == 'u' || c == 'o' || c == 'i' || c == 'y')
+	    {
+            ++count;
+	    }
+    }
+
+    cout << "Total number of vowels in file: " << count << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
